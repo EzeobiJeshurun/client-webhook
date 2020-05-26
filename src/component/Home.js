3d3bcadfb846b4c68cec4eba4b05cc70411e6b73
EzeobiJeshurun <jeshurunnonso@gmail.com>
2020-05-26 10:13:01 +0100

    setting up landing page

---
+++ src/component/Home.js
@@ -0,0 +1,35 @@
+import React, {useState, Fragment} from 'react';
+import {makeStyles}from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+    mainDiv: {
+        width: '100%',
+        backgroundColor : '#acf345',
+        height: '100vh',
+    }
+});
+
+function Home() {
+
+    const classes = useStyles();
+
+    //A random number generator, provides an integer id
+    const identityForPresentUser = Math.floor(Math.random()*10000000000);
+    //used to create and identify a specific channel or room for the communication
+    const  identityForMessageRoom = Math.floor(Math.random()*10000000000);
+
+    //the state below is used to identify the specific user interacting
+    //with the chat bot, it is sent to the server and user in the front-end as well
+    const [currentUser, setCurrentUser]= useState(identityForPresentUser);
+    //MessageRoom id sent to server to used in creating a session or room 
+    const [messageRoom, setMessageRoom]= useState(identityForMessageRoom);
+    return (
+        <Fragment>
+        <div className={classes.mainDiv}>
+            hello {currentUser} and {messageRoom}
+        </div>
+        </Fragment>
+    )
+}
+
+export default Home;
---
+++ src/component/Messenger.js
@@ -0,0 +1,11 @@
+import React, {Fragment,useState,useMemo} from 'react';
+
+function Messenger() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Messenger;
